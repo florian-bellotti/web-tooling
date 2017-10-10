6ab16e5303b92a8b4cb91180229f7bb068c5c6f2
QuentinHu <quentin.hu@thredup.com>
2017-10-10 16:06:17 +0200

    Rename app to client folder

--- src/client/app.routing.ts
+++ src/client/app.routing.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'pages', pathMatch: 'full' },
-  { path: 'pages', loadChildren: 'app/pages/pages.module#PagesModule' }
+  { path: 'pages', loadChildren: 'client/pages/pages.module#PagesModule' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes, {});
