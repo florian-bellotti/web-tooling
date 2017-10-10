6e549b036a0b8ad7f2fe426f8ea851634fd5fa8a
QuentinHu <quentin.hu@thredup.com>
2017-10-10 16:50:20 +0200

    Rename theme folder to common

--- src/client/pages/pages.module.ts
+++ src/client/pages/pages.module.ts
@@ -6,16 +6,16 @@ const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
 };
 import { ToastrModule } from 'ngx-toastr';
-import { DirectivesModule } from '../theme/directives/directives.module';
-import { PipesModule } from '../theme/pipes/pipes.module';
+import { DirectivesModule } from '../common/directives/directives.module';
+import { PipesModule } from '../common/pipes/pipes.module';
 import { routing } from './pages.routing';
 import { PagesComponent } from './pages.component';
 import { BlankComponent } from './blank/blank.component';
-import { MenuComponent } from '../theme/components/menu/menu.component';
-import { SidebarComponent } from '../theme/components/sidebar/sidebar.component';
-import { NavbarComponent } from '../theme/components/navbar/navbar.component';
-import { MessagesComponent } from '../theme/components/messages/messages.component';
-import { BackTopComponent } from '../theme/components/back-top/back-top.component';
+import { MenuComponent } from '../common/components/menu/menu.component';
+import { SidebarComponent } from '../common/components/sidebar/sidebar.component';
+import { NavbarComponent } from '../common/components/navbar/navbar.component';
+import { MessagesComponent } from '../common/components/messages/messages.component';
+import { BackTopComponent } from '../common/components/back-top/back-top.component';
 
 @NgModule({
   imports: [
