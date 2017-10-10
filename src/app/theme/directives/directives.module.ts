e41e318d9880f61d60a9a795236b072b212ceac1
QuentinHu <quentin.hu@thredup.com>
2017-10-10 15:41:26 +0200

    Fix errors - first pass

--- src/app/theme/directives/directives.module.ts
+++ src/app/theme/directives/directives.module.ts
@@ -3,29 +3,26 @@ import { CommonModule } from '@angular/common';
 
 import { SlimScroll } from './slim-scroll/slim-scroll.directive';
 import { Widget } from './widget/widget.directive';
-import { Skycon } from './skycon/skycon.directive';
 import { Counter } from './counter/counter.directive';
 import { LiveTile } from './live-tile/live-tile.directive';
 import { ProgressAnimate } from './progress-animate/progress-animate.directive';
 import { DropzoneUpload } from './dropzone/dropzone.directive';
 
 @NgModule({
-    imports: [ 
-        CommonModule 
+    imports: [
+        CommonModule
     ],
-    declarations: [ 
+    declarations: [
         SlimScroll,
         Widget,
-        Skycon,
         Counter,
         LiveTile,
         ProgressAnimate,
         DropzoneUpload
     ],
-    exports: [ 
+    exports: [
         SlimScroll,
         Widget,
-        Skycon,
         Counter,
         LiveTile,
         ProgressAnimate,
