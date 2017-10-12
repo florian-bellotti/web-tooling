a2de11e16df2b4957325d2ec851f87e598563687
QuentinHu <quentin.hu@thredup.com>
2017-10-12 10:58:40 +0200

    Rename routing

--- src/client/app.module.ts
+++ src/client/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { routing } from './app.routing';
+import { routing } from './routing';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/components/navbar/navbar.component';
